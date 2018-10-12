2da7ba1b3e810340b69c171b46012d5b3ef37954
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-12 23:21:15 +0200

    Fix typo in the migrations file

--- db/migrate/20181011235207_create_bank_accounts.rb
+++ db/migrate/20181011235207_create_bank_accounts.rb
@@ -2,7 +2,7 @@ class CreateBankAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :bank_accounts do |t|
       t.references :user, foreign_key: true
-      t.str :number
+      t.string :number
       t.decimal :balance
 
       t.timestamps

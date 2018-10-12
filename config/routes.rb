d011fe140ca3d8dd70d9073afd6af12690a7951c
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-12 23:59:15 +0200

    Scope all routes for devise users only

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users do
     get 'bank_accounts/show'
+    root to: 'bank_accounts#show'
   end
-  root to: 'bank_accounts#show'
 end

73e865ab7cb18aa38c98e0cf85efef145569f8b4
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-13 01:13:16 +0200

    Add percision, scale and default values to balance database migration

--- db/migrate/20181011235207_create_bank_accounts.rb
+++ db/migrate/20181011235207_create_bank_accounts.rb
@@ -3,7 +3,7 @@ class CreateBankAccounts < ActiveRecord::Migration[5.2]
     create_table :bank_accounts do |t|
       t.references :user, foreign_key: true
       t.string :number
-      t.decimal :balance
+      t.decimal :balance, precision: 10, scale: 2, default: 0
 
       t.timestamps
     end

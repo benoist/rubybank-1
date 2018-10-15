3a6ecad7185a87b9266d3906065479916ffb00dd
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-16 00:56:43 +0200

    Add bank accounts controller with create! and show methods

--- app/controllers/bank_accounts_controller.rb
+++ app/controllers/bank_accounts_controller.rb
@@ -1,5 +1,20 @@
 class BankAccountsController < ApplicationController
+  before_action :set_params, only: [:create!]
+
+  def create!(params)
+    bank_account = BankAccount.new(params)
+    warn "Creating an account with #{bank_account.attributes}"
+    bank_account.save!
+  end
+
   def show
     @account = current_user.bank_account
+    @records = current_user&.records
+  end
+
+  private
+
+  def set_params
+    params.require(:bank_account).permit(:id, :user_id, :created_at, :updated_at)
   end
 end

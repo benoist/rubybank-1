03fa01fb07c626fa2a2b65b599c56928754bfbbb
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-16 01:05:12 +0200

    Add relation between bank_account and record, generate bank account
    A bank account has many trensfer records.
    
    and each time we create a bank account, a bank account number will
    automatically be generated just before we commit to the database.

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,5 +1,13 @@
 class BankAccount < ApplicationRecord
   belongs_to :user
+  has_many :records, dependent: :destroy
   validates :user, presence: true
   validates :balance, numericality: { greater_than_or_equal_to: 0 }
+  before_commit :generate_bank_account_number, on: :create
+
+  private
+
+  def generate_bank_account_number
+    self.number = "ROR5BNK000#{id}" # For simplicity's sake, could be a SHA256!
+  end
 end

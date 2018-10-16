487bdbc34c6aceece6769c487ac12ccb3428df20
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-16 06:39:03 +0200

    [DONE] Refactor transfer model include all concerns slim down model

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,11 +1,26 @@
 class Transfer
-  def self.transfer(params)
-    sender ||= User.find(params[:sender].to_i)
-    recipient ||= User.find(params[:recipient].to_i)
-    amount ||= BigDecimal(params[:amount])
-    note ||= params[:note] if params[:note] != ''
-    ref ||= SecureRandom.hex(7)
+  include Reloadable
+  include AmountValidatable
+  include Recordable
+  include Transferable
 
+  attr_reader :sender, :recipient, :amount, :note, :ref
+
+  def initialize(params = {})
+    @sender ||= User.find(params[:sender].to_i)
+    @recipient ||= User.find(params[:recipient].to_i)
+    @amount ||= BigDecimal(params[:amount])
+    @note ||= params[:note] if params[:note] != ''
+    @ref = SecureRandom.hex(7)
+  end
+
+  def self.transfer(params = {})
+    new(params).send(:transfer)
+  end
+
+  private
+
+  def transfer
     warn "Transfer #{amount} from #{sender.name} to #{recipient.name}"
     return false unless amount_valid?(amount)
     begin
@@ -18,58 +33,10 @@ class Transfer
                        amount: amount, ref: ref, note: note)
       end
     rescue StandardError => e
-      warn "⚠️ #{([e.inspect]+e.backtrace).join($/)}"
+      warn "⚠️ #{([e.inspect] + e.backtrace).join($INPUT_RECORD_SEPARATOR)}"
       reload_accounts_and_records(sender, recipient)
       warn '❌ Transaction and objects state reverted!'
       raise
-    end
-  end
-
-  private
-
-  def self.amount_valid?(amount)
-    if amount <= 0
-      warn '❌ Transaction aborted! No negative amounts allowed.'
-      return false
-    end
-    true
-  end
-
-  def self.withdraw!(sender, amount)
-    warn "Withdraw #{amount} from #{sender.name}'s account: #{sender.bank_account&.number}"
-    adjust_balance_and_save!(sender.bank_account, -amount)
-  end
-
-  def self.deposit!(recipient, amount)
-    warn "Deposit #{amount} on #{recipient.name}'s account: #{recipient.bank_account&.number}"
-    adjust_balance_and_save!(recipient.bank_account, amount)
-  end
-
-  def self.adjust_balance_and_save!(bank_account, amount)
-    bank_account.balance += amount
-    bank_account.save!
-  rescue NoMethodError => e
-    raise StandardError, "Balance must at least be positive: #{e}"
-  end
-
-  def self.create_record!(opts = {})
-    Record.create!(
-      status: opts[:status],
-      amount: opts[:amount],
-      note: opts[:note],
-      reference: opts[:ref],
-      bank_account_id: opts[:user].bank_account.id,
-      counterpart_id: opts[:counterpart].bank_account.id
-    )
-  end
-
-  def self.reload_accounts_and_records(sender, recipient)
-    # this method keeps the memory and database in-sync after a rollback event!
-    # if there are no records that correspond at all it will silently go on
-    # because there is nothing to reload.
-    [sender, recipient].each do |user|
-      user.reload&.bank_account
-      user.reload&.bank_account&.records
     end
   end
 end

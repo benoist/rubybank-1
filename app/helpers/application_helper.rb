9ca1ff8efcab143531e004a798670fac5c8b609b
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-16 07:03:40 +0200

    Add amount_format helper and improve views. all DONE!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def amount_formater(amount)
+    if amount < 0
+      format '%.2f', amount.round(2)
+    elsif amount > 0
+      "+#{format('%.2f', amount.round(2))}"
+    end
+  end
 end

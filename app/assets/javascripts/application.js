66c5cb0181d48a1abc70a6e55ada4105bdb17455
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-16 02:35:33 +0200

    Fix assets (js, css, scss, jquery, bootstrap) and Fix gems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,12 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//= require jquery3
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
+//= require jquery-ui
+
+

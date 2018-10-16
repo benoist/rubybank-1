8668ed22630f0b860a286dc1cac7a16b60ed9941
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-16 02:28:40 +0200

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = %w[user1 user2 user3 user4]
+users.each_with_index do |user, i|
+  User.create!(
+               name: user.humanize,
+               email: "#{user.downcase}@rubybank.com",
+               password: '123456'
+              )
+  BankAccount.create!(
+                      user_id: i+1,
+                      number: "ROR5BNK000#{i}",
+                      balance: '2000'
+                     )
+end

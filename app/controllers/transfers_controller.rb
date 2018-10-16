b57f0ec7078e9be0e2699eed7b877044304fadb0
Khalil Gharbaoui <khalilgharbaoui@hotmail.com>
2018-10-16 02:16:34 +0200

    Add transfers_controller
    In this controller we have 2 main methods.
    - new:
    provides the transfer form with @users instance variable,
    we allready have current_user availeble and we dont want to
    transfer to ourself. so we do: where.not
    Now we can choose only the other users to transfer to.
    
    - create!:
    
    This method passes the params to the Transfer model transfer class method
    and when its done and is saved it redirect the user to the root_path
    or we receive an exception from it.
    
    either:
      1. the record is not found.
      2. we did not fill in the amount
      3. any other exeption.
    we redirect the user to form and trigger a flash alert.

---
+++ app/controllers/transfers_controller.rb
@@ -0,0 +1,32 @@
+class TransfersController < ApplicationController
+  def new
+    @users = User.where.not(id: current_user.id)
+  end
+
+  def create
+    @transfer = Transfer.transfer(transfer_params)
+
+    respond_to do |format|
+      if @transfer.save
+        format.html { redirect_to root_path, notice: 'Transfered successfully.' }
+      else
+        format.html { render :new }
+      end
+    end
+  rescue ActiveRecord::RecordNotFound => e
+    warn "🛑 #{([e.inspect]+e.backtrace).join($/)}"
+    redirect_to '/transfers/new', alert: "Record not found: #{e}"
+  rescue ArgumentError => e
+    warn "🛑 #{([e.inspect]+e.backtrace).join($/)}"
+    redirect_to '/transfers/new', alert: "Amount can not be empty! #{e}"
+  rescue StandardError => e
+    warn "🛑 #{([e.inspect]+e.backtrace).join($/)}"
+    redirect_to '/transfers/new', alert: e
+  end
+
+  private
+
+  def transfer_params
+    params.permit(:sender, :recipient, :amount, :note)
+  end
+end
